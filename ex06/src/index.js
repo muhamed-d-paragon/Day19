0165a713728cb0a678230e885f94449ad62fb2bd
unknown <muhamed.webdevelopment@gmail.com>
2021-03-27 09:26:11 +0100

    React 4

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-class App extends Component {
+class App extends React.Component {
   render() {
+    const World = "World";
     return (
       <div className="App">
-        <h1>Hello, Padawans!</h1>
+        <h1>Hello, {World}!</h1>
       </div>
     )
   }
